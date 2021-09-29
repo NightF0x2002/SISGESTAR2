1b4330148fe552ce5017222492be3be9a5ee99b6
Felipe Barcellos <felipe.reis@basis.com.br>
2021-09-29 15:47:33 -0300

    criação das camadas do spring

---
+++ src/main/java/com/github/nightf0x2002/SISGESTAR2/service/dto/TarefaListDTO.java
@@ -0,0 +1,2 @@
+package com.github.nightf0x2002.SISGESTAR2.service.dto;public class TarefaListDTO {
+}
---
+++ src/main/java/com/github/nightf0x2002/SISGESTAR2/service/mapper/TarefaMapper.java
@@ -0,0 +1,4 @@
+package com.github.nightf0x2002.SISGESTAR2.service.mapper;
+
+public interface TarefaMapper {
+}
