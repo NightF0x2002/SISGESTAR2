5b96269ecc898d99408da7bd79d1b2e529c75018
Felipe Barcellos <felipe.reis@basis.com.br>
2021-09-29 12:46:18 -0300

    primeiros arquivos

---
+++ src/main/java/com/github/nightf0x2002/SISGESTAR2/Sisgestar2Application.java
@@ -0,0 +1,13 @@
+package com.github.nightf0x2002.SISGESTAR2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sisgestar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sisgestar2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nightf0x2002/SISGESTAR2/Sisgestar2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nightf0x2002.SISGESTAR2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sisgestar2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
