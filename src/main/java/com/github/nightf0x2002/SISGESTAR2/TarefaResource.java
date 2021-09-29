3da3f52e544ba27624124f0870fa3b7fe022aee0
Felipe Barcellos <felipe.reis@basis.com.br>
2021-09-29 13:21:38 -0300

    acerto de arquivos

--- src/main/java/com/github/nightf0x2002/SISGESTAR2/TarefaResource.java
+++ src/main/java/com/github/nightf0x2002/SISGESTAR2/TarefaResource.java
@@ -1,2 +1,28 @@
-package com.github.nightf0x2002.SISGESTAR2;public class TarefaResource {
+package com.github.nightf0x2002.SISGESTAR2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tarefas")
+public class TarefaResource {
+
+    List<Tarefa> tarefas = new ArrayList<>();
+
+    @GetMapping
+    public List<Tarefa> recuperarTodos() {
+        return tarefas;
+    }
+
+    @PostMapping
+    public Tarefa criarTarefa(@RequestBody Tarefa novaTarefa) {
+        tarefas.add(novaTarefa);
+        return novaTarefa;
+    }
 }
